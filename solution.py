56584bc0051f3ddbadf613cfaf07971da6c66d2d
Andrey Yurlov <76644125+xvrfcsxul@users.noreply.github.com>
2024-05-24 16:48:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 399364172
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(sample1: np.array, sample2: np.array) -> bool:
+    ks_stat, p_value = stats.ks_2samp(sample1, sample2)
+    
+    return p_value > 0.07:
