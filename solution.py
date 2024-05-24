54576be40c3fbdca36e4f5a6de5c04ba4a02e318
Andrey Yurlov <76644125+xvrfcsxul@users.noreply.github.com>
2024-05-24 16:49:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,4 @@ chat_id = 399364172
 def solution(sample1: np.array, sample2: np.array) -> bool:
     ks_stat, p_value = stats.ks_2samp(sample1, sample2)
     
-    return p_value > 0.07:
+    return p_value < 0.07
